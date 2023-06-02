3922a86fa4a1b50a8c04993d43d26f3e43433f94
inkma <115982948+tomaslmz@users.noreply.github.com>
2023-06-02 15:51:23 -0300

    feat: registro e login

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const app = express();
 const handlebars = require('express-handlebars');
 const bodyParser = require('body-parser');
 
+const Usuarios = require('./model/Usuarios');
+const Postagens = require('./model/Postagens');
+
 app.engine("handlebars", handlebars.engine({
     defaultLayout: 'main',
     
@@ -15,20 +18,52 @@ app.engine("handlebars", handlebars.engine({
 }));
 
 app.set("view engine", "handlebars");
-
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.post('/', (req, res) => {
+app.get('/', (req, res) => {
     res.render('start');
 });
 
+app.get('/registro', (req, res) => {
+    res.render("register");
+});
+
+app.post('/registrar', (req, res) => {
+    Usuarios.create({
+        nome: req.body.nome,
+        senha: req.body.senha,
+        email: req.body.email
+    }).then(() => {
+        res.redirect('/');
+    }).catch((erro) => {
+        res.send("ERRO NO REGISTRO DO USUÁRIO!<br>+" + erro);
+    });
+});
+
+app.get('/login', (req, res) => {
+    res.render("login");
+});
+
+app.post('/logar', async (req, res) => {
+    // Usuarios.findOne({where: {'senha' : req.body.senha, 'email' : req.body.email}}).then(() => {
+    //     res.redirect("/inicio");
+    // }).catch((erro) => {
+    //     res.send("ERRO AO LOGAR: " + erro);
+    // });
+    const user = await Usuarios.findOne({where: {'email' : req.body.email,'senha' : req.body.senha}});
+
+    if(!user) {
+        res.redirect("/login");
+    } else {
+        res.redirect("/inicio");
+    }
+});
+
+
 app.get("/inicio", (req, res) => {
     res.render('inicio');
-})
-
-
-
+});
 
 
 
